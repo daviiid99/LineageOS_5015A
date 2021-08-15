c94461b05e5d9b6884a82f2de89608e4d964d109
David <davidmolinamorales1999@gmail.com>
2021-08-15 23:26:44 +0200

    Update lineage_pixi.mk

--- lineage_pixi.mk
+++ lineage_pixi.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from k-touch i9 device
-$(call inherit-product, device/tcl/5015A/device.mk)
+$(call inherit-product, device/tcl/5015a/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
