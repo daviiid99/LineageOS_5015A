fd581d942d91848b5f6e86d1738b67ad2aa89c4a
David <davidmolinamorales1999@gmail.com>
2021-08-15 21:23:02 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,1 +1,114 @@
+DEVICE_PATH := device/tcl/5015A
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/tcl/5015A/pixi-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+include vendor/mediatek/hardware/telephony-ext/overlay.mk
+
+# A/B
+AB_OTA_UPDATER := false
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+    
+    # Lights
+PRODUCT_PACKAGES += \
+    lights.mt6580
+    
+ # Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6739:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6739
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mt6739.rc \
+    init.wifi.mt6739.sh \
+    fstab.enableswap
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_mediatek_video.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.secure_lock_screen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.secure_lock_screen.xml
+
+# Wifi
+PRODUCT_PACKAGES += \
+    lib_driver_cmd_mt66xx \
+    libwifi-hal-mt66xx \
+    wifi_hal \
+    libwpa_client \
+    hostapd \
+    hostapd_cli \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
+    
+    # FMRadio
+PRODUCT_PACKAGES += \
+    FMRadio \
+    libfmcust \
+    libfmjni \
+    libfmmt6620 \
+    libfmmt6627 \
+    libfmmt6628 \
+    libfmmt6630 \
+    libmtkplayer
+    
+    # GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
+
+PRODUCT_PACKAGES += \
+    gps.mt6580 \
+    YGPS
+    
+    # Bluetooth
+PRODUCT_PACKAGES += \
+    libldacBT_dec \
+    libbtconfigstore
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+    mtk-telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+    
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
+    
+ # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
