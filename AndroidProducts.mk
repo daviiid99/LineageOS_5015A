826680be066e169c7406281e1c6ade1c833b256e
David <davidmolinamorales1999@gmail.com>
2021-08-15 23:32:42 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pixi.mk
+    $(LOCAL_DIR)/lineage_5015a.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pixi-user \
-    lineage_pixi-userdebug \
-    lineage_pixi-eng
+    lineage_5015a-user \
+    lineage_5015a-userdebug \
+    lineage_5015a-eng
