aea5e800b8a0ca16c9c020f4106ed4749984b7cf
David <davidmolinamorales1999@gmail.com>
2021-08-22 16:54:47 +0200

    Adding COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_5015a.mk
+    
+COMMON_LUNCH_CHOICES := \
+    lineage_5015a-eng \
+    lineage_5015a-user \
+    lineage_5015a-userdebug
