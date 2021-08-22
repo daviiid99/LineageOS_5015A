fd697bf71a1dee071e41fe8e06c76d512ee4f16e
David <davidmolinamorales1999@gmail.com>
2021-08-22 17:12:03 +0200

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6580.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.gts7xlwifi.rc
+LOCAL_SRC_FILES    := etc/init.mt6580.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
